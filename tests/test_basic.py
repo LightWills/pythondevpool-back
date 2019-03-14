9b753079169460664d242bd3015bc090d9687d96
Tony Clonier <clonier.tony@hotmail.fr>
2019-03-14 13:55:31 +0100

    Adding test should work now

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,9 +4,8 @@ import unittest
 class BasicTestCase(unittest.TestCase):
 
     def test_add(self):
-        self.assert(1, 2)
+        self.assert(1, 1)
 
 
 if __name__ == "__main__":
     unittest.main()
-    
